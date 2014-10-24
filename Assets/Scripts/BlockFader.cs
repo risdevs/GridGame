96f6ac1386e7d38be3a72401753cbf141d3df019
Sergi Velez <ris@risdevs.com>
2014-10-24 19:17:15 +0200

    add blokc fader

---
+++ Assets/Scripts/BlockFader.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent( typeof( SpriteRenderer ), typeof(BoxCollider2D ) )]
+public class BlockFader : MonoBehaviour
+{
+	public bool hideCollider = false;
+	public float smoothFadingFactor = 1;
+
+	private bool hiding;
+	private Color transparentColor;
+	private Color realColor;
+	private SpriteRenderer spriteRenderer;
+
+
+    // Use this for initialization
+    void Start()
+    {
+		spriteRenderer = GetComponent<SpriteRenderer> ();
+		hiding = true;
+		realColor = spriteRenderer.color;
+		transparentColor = realColor;
+		transparentColor.a = 0;
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+		spriteRenderer.color = Color.Lerp(spriteRenderer.color, (hiding ? transparentColor : realColor), Time.deltaTime * smoothFadingFactor);
+
+		if (shouldFlip ()) 
+		{
+			hiding = !hiding;
+		}
+
+
+		if (hideCollider)
+		{
+			GetComponent<BoxCollider2D> ().enabled = spriteRenderer.color.a > 0.2f;
+		}
+    }
+
+    public bool shouldFlip()
+    {
+		float dist = hiding ? spriteRenderer.color.a - transparentColor.a : spriteRenderer.color.a - realColor.a;
+
+		return Mathf.Abs (dist) < 5e-2f;
+    }
+}
