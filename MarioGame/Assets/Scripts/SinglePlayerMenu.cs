6101a6a9e42a5aff59bf6dca4d977b7e4b50130a
Marc Canaleta <marc.canaleta@socialpoint.es>
2014-10-24 17:18:22 +0200

    wip

---
+++ MarioGame/Assets/Scripts/SinglePlayerMenu.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SinglePlayerMenu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void ClickBack() {
+        Application.LoadLevel("Main");
+    }
+
+    public void ClickLevel(int level) {
+
+    }
+}
