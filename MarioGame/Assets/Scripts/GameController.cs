56c7743a624eb4e49ae902595cb6e86100204067
Sergi Velez <ris@risdevs.com>
2014-10-24 17:10:36 +0200

    skeleton done

---
+++ MarioGame/Assets/Scripts/GameController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        //Application.LoadLevelAdditive("Level2");
+        
+        Application.LoadLevelAdditive("Level" + Random.Range(1,3));
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+    
+    }
+}
--- MarioGame/Assets/Scripts/PlayerController.cs
+++ MarioGame/Assets/Scripts/PlayerController.cs
@@ -1,35 +1,80 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
 
     public int horizontalForce, verticalForce;
+    private bool grounded;
+   
+    // Use this for initialization
+    void Start()
+    {
+    
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        HandleInput();
+        EnforceBounds();
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        handleInput();
-	}
+        if (this.rigidbody2D.velocity.y == 0 /*&& transform.position.y < -3.5*/)
+        {
+            grounded = true;
+        }
+    }
 
-    private void handleInput()
+    private void HandleInput()
     {
         if (Input.GetKey("left"))
         {
             this.rigidbody2D.AddForce(new Vector2(-horizontalForce, 0f));
+
+            if (this.transform.localScale.x > 0)
+                this.transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y);
         }
         
         if (Input.GetKey("right"))
         {
             this.rigidbody2D.AddForce(new Vector2(horizontalForce, 0f));
+
+            if (this.transform.localScale.x < 0)
+                this.transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y);
         }
         
-        if (Input.GetKey("up"))
+        if (Input.GetKey("up") && grounded)
         {
             this.rigidbody2D.AddForce(new Vector2(0f, verticalForce));
+            grounded = false;
         }
+    }
+
+    private void EnforceBounds()
+    {
+        Vector3 newPosition = transform.position; 
+        Camera mainCamera = Camera.main;
+        Vector3 cameraPosition = mainCamera.transform.position;
+
+        float xDist = mainCamera.aspect * mainCamera.orthographicSize; 
+        float xMax = cameraPosition.x + xDist;
+        float xMin = cameraPosition.x - xDist;
+
+        float height = GetComponent<SpriteRenderer>().sprite.rect.size.y;
+        float yDist = mainCamera.orthographicSize; 
+        float yMax = cameraPosition.y + yDist;
+        float yMin = cameraPosition.y - yDist;
+        
+        if (newPosition.x < xMin || newPosition.x > xMax)
+        {
+            newPosition.x = Mathf.Clamp(newPosition.x, xMin, xMax);
+        }
+
+        if (newPosition.y < yMin || newPosition.y > yMax)
+        {
+            newPosition.y = Mathf.Clamp(newPosition.y, yMin, yMax);
+        }
+
+        transform.position = newPosition;
     }
 }
