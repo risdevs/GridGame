4ebb942abdcb7d8140a9108a7560ed7899f9fdf3
Sergi Velez <ris@risdevs.com>
2014-10-24 16:14:26 +0200

    skeleton

---
+++ MarioGame/Assets/Block.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Block : MonoBehaviour
+{
+
+    public Sprite[] sprites;
+    public int currentSprite;
+
+    // Use this for initialization
+    void Start()
+    {
+    
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {        GetComponent<SpriteRenderer>().sprite = sprites [currentSprite];
+    }
+}
---
+++ MarioGame/Assets/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public int horizontalForce, verticalForce;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        handleInput();
+	}
+
+    private void handleInput()
+    {
+        if (Input.GetKey("left"))
+        {
+            this.rigidbody2D.AddForce(new Vector2(-horizontalForce, 0f));
+        }
+        
+        if (Input.GetKey("right"))
+        {
+            this.rigidbody2D.AddForce(new Vector2(horizontalForce, 0f));
+        }
+        
+        if (Input.GetKey("up"))
+        {
+            this.rigidbody2D.AddForce(new Vector2(0f, verticalForce));
+        }
+    }
+}
